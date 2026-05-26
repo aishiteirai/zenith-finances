24eaca753dd2a339741946b84080a297ee323c01
Ryan Corazza Alvarenga <ryancorazzaalvarenga@gmail.com>
2026-05-26 20:52:25 -0300

    update: carteira repository

--- src/main/java/com/example/zenith/repository/CarteiraRepository.java
+++ src/main/java/com/example/zenith/repository/CarteiraRepository.java
@@ -2,14 +2,10 @@ package com.example.zenith.repository;
 
 import com.example.zenith.model.Carteira;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface CarteiraRepository extends JpaRepository<Carteira, Long> {
-
     List<Carteira> findByInvestidorEmail(String email);
 
     Optional<Carteira> findByIdAndInvestidorEmail(Long id, String email);
